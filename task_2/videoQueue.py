ea54286bb9e8d2b30fb30db327257e5876673ef1
JWangNov <wangjialun@126.com>
2020-02-23 00:58:11 -0500

    Create videoQueue.py

---
+++ task_2/videoQueue.py
@@ -0,0 +1,55 @@
+import queue
+import threading
+from videoGenerator import VideoGen
+
+
+class VideoQueue():
+    def __init__(self, pipelineNum, queueSize):
+        self.pipelineNum = pipelineNum
+        self.queueSize = queueSize
+        self.processingItems = []
+        self.myQueue = queue.Queue(queueSize)
+
+    def getQueued(self, username, howManyTweets):
+        currT = threading.Thread(target=self.genCurrVideo, args=(username, howManyTweets))
+        processingNum = threading.active_count() - 1
+        if processingNum < self.pipelineNum:
+            currT.run()
+            self.processingItems.append(currT)
+        else:
+            self.myQueue.put(currT)
+            print(f"{username} is in queue now.")
+        return currT
+
+    def genCurrVideo(self, username, howManyTweets):
+        currVideo = VideoGen(username, howManyTweets)
+        print(f"Start generating {username}'s video.")
+        currVideo.run()
+        print(f"{username} DONE")
+
+    def run(self):
+        while not self.myQueue.empty():
+            processingNum = threading.active_count()
+            if processingNum < self.pipelineNum:
+                t = self.myQueue.get()
+                t.run()
+        for eachItem in self.processingItems:
+            if eachItem.is_alive():
+                eachItem.join()
+        return 0
+
+
+def main():
+    pipelineNum = 2
+    queueSize = 100
+    obj = VideoQueue(pipelineNum, queueSize)
+    obj.getQueued("@LagavulinWhisky", 10)
+    obj.getQueued("@Laphroaig", 10)
+    obj.getQueued("@Kilchomanwhisky", 10)
+    obj.getQueued("@Benromach", 10)
+    obj.getQueued("@clydesidewhisky", 10)
+    obj.run()
+
+
+if __name__ == '__main__':
+    main()
